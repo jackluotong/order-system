06cb2ae8d2d187445669f07db5e87d43a6a3d057
williamtong <jackluotong@outlook.com>
2021-11-09 11:10:53 +0800

    add file
    for params

---
+++ src/config/PARAMS.js
@@ -0,0 +1,10 @@
+/*
+ * @Description:
+ * @version: 1.0.0
+ * @Author: william
+ * @Date: 2021-11-08 17:41:47
+ * @LastEditors: william
+ * @LastEditTime: 2021-11-08 17:41:48
+ */
+
+const appid = 'wxaf4dcaf892c284d5'
