38a55e259036bb4009eb785d61794904f3b1f3af
william <jackluotong@outlook.com>
2021-11-09 16:21:34 +0800

    update router config
    config and router

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,11 @@
  * @Author: william
  * @Date: 2021-11-03 11:17:01
  * @LastEditors: william
+<<<<<<< refs/remotes/origin/v1.0.0
  * @LastEditTime: 2021-11-09 18:38:48
+=======
+ * @LastEditTime: 2021-11-09 15:22:54
+>>>>>>> update router config
  * @For What?: 
 -->
 <script>
@@ -13,10 +17,14 @@ export default {
         console.log('App Launch')
     },
     onShow: function () {
+<<<<<<< refs/remotes/origin/v1.0.0
         console.log('App Show', this.globalData)
+=======
+        console.log('App Show', ROUTES, '-----', uni)
+>>>>>>> update router config
     },
     onHide: function () {
-        console.log('App Hide', ROUTES)
+        console.log('App Hide')
     },
     globalData: {
         name: 'jack',
--- src/config/PARAMS.js
+++ src/config/PARAMS.js
@@ -2,9 +2,16 @@
  * @Description:
  * @version: 1.0.0
  * @Author: william
+<<<<<<< refs/remotes/origin/v1.0.0
  * @Date: 2021-11-08 17:41:47
  * @LastEditors: william
  * @LastEditTime: 2021-11-08 17:41:48
  */
 
 const appid = 'wxaf4dcaf892c284d5'
+=======
+ * @Date: 2021-11-09 15:18:34
+ * @LastEditors: william
+ * @LastEditTime: 2021-11-09 15:18:35
+ */
+>>>>>>> update router config
